4c14e538018bb132efe27b597993e0da21187e5a
oleksiirude <oleksiirudenkomail@gmail.com>
2019-01-21 20:39:12 +0200

    lol

---
+++ ft_major_parsing.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parsing_1st_part.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olrudenk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/08 17:52:30 by olrudenk          #+#    #+#             */
+/*   Updated: 2019/01/14 14:52:31 by olrudenk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+size_t	ft_main_parse(char **fmt, t_pmts **params)
+{
+	while (!ft_strchr("diouxXcspf%", **fmt) && **fmt)
+	{
+		if (ft_strchr("#+- .0123456789hlL", **fmt))
+			ft_rec_params(fmt, params);
+		else
+			return (0);
+		(*fmt)++;
+	}
+	if (**fmt == PERC)
+	{
+		(*fmt)++;
+		(*params)->type = PERC;
+		return (1);
+	}
+	if (**fmt)
+	{
+		(*params)->type = **fmt;
+		(*fmt)++;
+		return (2);
+	}
+	return (0);
+}
+
+t_prts	*ft_processing(va_list ap, char **fmt)
+{
+	size_t result;
+	t_pmts *pmts;
+
+	pmts = ft_set_flags_to_zero();
+	result = ft_main_parse(fmt, &pmts);
+	if (!result)
+		return (ft_invalid_str_form(fmt, &pmts));
+	else if (result == 1)
+		return (ft_valid_str_form(ap, pmts, fmt));
+	else
+		return (ft_valid_str_form(ap, pmts, fmt));
+}
---
+++ ft_minor_parsing.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parsing_2nd_part.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olrudenk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/08 17:52:30 by olrudenk          #+#    #+#             */
+/*   Updated: 2019/01/14 14:52:31 by olrudenk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_prec_case(char **fmt, t_pmts **params)
+{
+	(*params)->prec = 1;
+	if ((*(*fmt) + 1) >= '0' || (*(*fmt) + 1) <= '9')
+	{
+		(*fmt)++;
+		(*params)->prec_value = ft_atoi_light(fmt);
+	}
+}
+
+void	ft_zero_case(char **fmt, t_pmts **params)
+{
+	(*params)->zero = 1;
+	if ((*(*fmt) + 1) >= '0' || (*(*fmt) + 1) <= '9')
+		(*params)->zero_value = ft_atoi_light(fmt);
+}
+
+void	ft_h_or_hh_case(char **fmt, t_pmts **params)
+{
+	if (*(*fmt + 1) == 'h')
+	{
+		(*params)->mod = HH;
+		(*fmt)++;
+	}
+	else
+		(*params)->mod = H;
+}
+
+void	ft_l_or_ll_case(char **fmt, t_pmts **params)
+{
+	if (*(*fmt + 1) == 'l')
+	{
+		(*params)->mod = LL;
+		(*fmt)++;
+	}
+	else
+		(*params)->mod = L;
+}
+
+void	ft_rec_params(char **fmt, t_pmts **params)
+{
+	if (**fmt == '.')
+		ft_prec_case(fmt, params);
+	else if (**fmt == '0')
+		ft_zero_case(fmt, params);
+	else if (**fmt == H)
+		ft_h_or_hh_case(fmt, params);
+	else if (**fmt == L)
+		ft_l_or_ll_case(fmt, params);
+	else if (**fmt >= '1' && **fmt <= '9')
+		(*params)->value = ft_atoi_light(fmt);
+	else if (**fmt == '#')
+		(*params)->hash = 1;
+	else if (**fmt == '+')
+		(*params)->plus = 1;
+	else if (**fmt == '-')
+		(*params)->minus = 1;
+	else if (**fmt == ' ')
+		(*params)->space = 1;
+	else if (**fmt == L)
+		(*params)->mod = LBIG;
+}
+
